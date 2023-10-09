cfcc9ae44a9925b06ec1e29635777330633e12cf
Yury Kuznetsov <yury-kuznetsov@mail.ru>
2023-10-10 04:03:26 +0500

    Подключение uricoder к серверу

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,10 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/yury-kuznetsov/shortener/internal/app"
+)
+
+func handler(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		handlers.HandlerPost(res, req)
+		return
+	}
+
+	if req.Method == http.MethodGet {
+		handlers.HandlerGet(res, req)
+		return
+	}
+
+	http.Error(res, "only GET/POST requests are allowed", http.StatusBadRequest)
+}
 
 func main() {
-	err := http.ListenAndServe(`:8080`, nil)
-	if err != nil {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, handler)
+
+	if err := http.ListenAndServe(`:8080`, mux); err != nil {
 		panic(err)
 	}
 }
---
+++ internal/app/handlers.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/yury-kuznetsov/shortener/cmd/storage"
+	"github.com/yury-kuznetsov/shortener/cmd/uricoder"
+)
+
+func HandlerGet(res http.ResponseWriter, req *http.Request) {
+	code := strings.TrimLeft(req.URL.Path, "/")
+	uri, err := getCoder().ToURI(code)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+	http.Redirect(res, req, uri, http.StatusTemporaryRedirect)
+}
+
+func HandlerPost(res http.ResponseWriter, req *http.Request) {
+	uri, _ := io.ReadAll(req.Body)
+	code, err := getCoder().ToCode(string(uri))
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+	res.Header().Set("content-type", "text/plain")
+	res.WriteHeader(http.StatusCreated)
+	_, _ = res.Write([]byte("http://localhost:8080/" + code))
+}
+
+func getCoder() *uricoder.Coder {
+	return uricoder.NewCoder(storage.ArrStorage)
+}
