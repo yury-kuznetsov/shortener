45151ea2a590f54993191709413e3971d524d2b6
Yury Kuznetsov <yury-kuznetsov@mail.ru>
2023-10-14 20:47:44 +0500

    Добавление переменных окружения

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var Options struct {
 	HostAddr string
@@ -9,10 +12,20 @@ var Options struct {
 
 func Init() {
 	initFlags()
+	initEnv()
 }
 
 func initFlags() {
 	flag.StringVar(&Options.HostAddr, "a", ":8080", "TCP network address")
 	flag.StringVar(&Options.BaseAddr, "b", "http://localhost:8080", "base address")
 	flag.Parse()
+}
+
+func initEnv() {
+	if envHostAddr := os.Getenv("SERVER_ADDRESS"); envHostAddr != "" {
+		Options.HostAddr = envHostAddr
+	}
+	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
+		Options.BaseAddr = envBaseAddr
+	}
 }
